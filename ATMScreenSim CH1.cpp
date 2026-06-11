9ae80be8604c163760d112f72e95e6e15e03f66a
mylesfields7-sketch <mylesfields7@gmail.com>
2026-06-11 00:31:32 -0400

    Add files via upload

---
+++ ATMScreenSim CH1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "-------------------------" << endl;
+	cout << "     WELCOME TO ATM     " << endl;
+	cout << "-------------------------" << endl;
+	cout << "1. Balance Inquiry" << endl;
+	cout << "2. Cash Withdrawal" << endl;
+	cout << "3. Deposit" << endl;
+	cout << "4. Mini Statement" << endl;
+	cout << "5. Exit" << endl;
+	cout << "-------------------------" << endl;
+	cout << "Please insert your card" << endl;
+
+	return 0;
+}
