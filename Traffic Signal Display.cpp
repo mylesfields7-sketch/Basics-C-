27e61e27a51d076d5c5f3dd327cbaccc0307f401
mylesfields7-sketch <mylesfields7@gmail.com>
2026-06-10 23:35:52 -0400

    Add files via upload

---
+++ Traffic Signal Display.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "TRAFFIC SIGNAL" << endl;
+	cout << "--------------" << endl;
+	cout << "RED    : STOP" << endl;
+	cout << "YELLOW : READY" << endl;
+	cout << "GREEN  : GO" << endl;
+
+	return 0;
+}
