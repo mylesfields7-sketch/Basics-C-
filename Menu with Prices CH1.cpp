92ec986e2d9832b295624ae3e746c010914d6ec7
mylesfields7-sketch <mylesfields7@gmail.com>
2026-06-10 22:56:26 -0400

    Add files via upload

---
+++ Menu with Prices CH1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Whyaburger" << endl;
+	cout << endl;
+	cout << endl;
+	cout << "Burger Meal     9.99" << endl;
+	cout << "  includes one soft drink and one" << endl;
+	cout << "french fries" << endl;
+	cout << "Burger Only     5.69" << endl;
+	cout << "Soft Drink      1.99" << endl;
+	cout << "French Fries    2.29" << endl;
+
+	return 0;
+}
