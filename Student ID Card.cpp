2d3f849d2abb032e8b0a599cfe85c68713a08a70
mylesfields7-sketch <mylesfields7@gmail.com>
2026-06-10 23:09:21 -0400

    Add files via upload

---
+++ Student ID Card.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+	cout << "Name: John Stewville" << endl;
+	cout << "Course:Computer Science" << endl;
+	cout << "ID No: 123456" << endl;
+	cout << "College: Auburn University at Montgomery" << endl;
+
+	return 0;
+}
