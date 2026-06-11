fe6febad0b8ea84e886c13cbffdc3fd5a02ebd83
mylesfields7-sketch <mylesfields7@gmail.com>
2026-06-11 18:52:55 -0400

    Add files via upload

---
+++ Block Style.cpp
@@ -0,0 +1,18 @@
+// A Hello World Program
+#include <iostream>
+using namespace std;
+
+
+int main()
+
+{
+	//Printing the word "CODE" in block style
+	cout << "CCCC   OOO  DDDD   EEEEE " << endl;
+	cout << "C     O   O D   D  E   " << endl;
+	cout << "C     O   O D    D EEEE  " << endl;
+	cout << "C     0   O D   D  E   " << endl;
+	cout << "CCCC   OOO  DDDD   EEEEE   " << endl;
+
+
+	return 0;
+}
