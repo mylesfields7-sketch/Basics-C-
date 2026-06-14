976f83446a85a89ea245331cb55152214da72136
mylesfields7-sketch <mylesfields7@gmail.com>
2026-06-13 21:54:24 -0400

    Add files via upload

---
+++ Arithmetic Operations.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int first_number;
+	first_number = 15;
+
+	int second_number;
+	second_number = 10;
+
+	int result;
+	result = first_number + second_number;
+	cout << "Result is: " << result << endl;
+
+	return 0;
+}
