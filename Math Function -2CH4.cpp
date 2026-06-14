6a50b5d21afdcd32eedcc09a18e719904a06a6e8
mylesfields7-sketch <mylesfields7@gmail.com>
2026-06-14 15:37:28 -0400

    Add files via upload

---
+++ Math Function -2CH4.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // for cout, endl
+#include <cmath> // for pow()
+using namespace std;
+
+int main()
+
+{
+
+	double result;
+	result = pow(3, 4);
+	cout << "3 raised to 4 is " << result << endl;
+
+	return 0;
+
+}
