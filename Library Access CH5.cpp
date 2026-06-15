8597f67cde4dffce2ffe557bc673db8b4233c8d0
mylesfields7-sketch <mylesfields7@gmail.com>
2026-06-14 23:06:46 -0400

    Add files via upload

---
+++ Library Access CH5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	bool hasID = true;
+	bool libraryClosed = false;
+
+	if (hasID && !libraryClosed) {
+		cout << "Access denied." << endl;
+	}
+	else {
+		cout << "Access denied." << endl;
+	}
+
+	return 0;
+}
