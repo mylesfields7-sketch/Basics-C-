75aaf56f9e64f16d06c99b0832aa187a4cbbe59e
mylesfields7-sketch <mylesfields7@gmail.com>
2026-06-14 22:03:54 -0400

    Add files via upload

---
+++ Comparing Alphabets CH5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char letter1 = 'b';
+	char letter2 = 'a';
+
+	if (letter1 == letter2)
+	{
+		cout << "Both letters are the same" << endl;
+	}
+
+	else
+	{
+		cout << "Both letters are different" << endl;
+	}
+
+	return 0;
+}
