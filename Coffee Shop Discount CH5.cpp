4e43c9da273fce9c58dccfadf45a25049fe14f28
mylesfields7-sketch <mylesfields7@gmail.com>
2026-06-14 23:17:02 -0400

    Add files via upload

---
+++ Coffee Shop Discount CH5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double totalBill = 22.50;
+
+	if (totalBill >= 20) {
+		cout << "Free cookie included!" << endl;
+	}
+	else {
+		cout << "Thank you for shopping!" << endl;
+	}
+
+	return 0;
+}
