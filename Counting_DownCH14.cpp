ce2f253849fac1ef161c6c3dc946ab13b6a0b49b
mylesfields7-sketch <mylesfields7@gmail.com>
2026-07-10 15:21:17 -0400

    Add files via upload

---
+++ Counting_DownCH14.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 5; i >= 1; i--)
+	{
+		cout << i << " ";
+	}
+	return 0;
+}
---
+++ Print_1_to_NCH14.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number;
+	cout << "Enter N: ";
+	cin >> number;
+
+	for (int i = 1; i <= number; i++)
+	{
+		cout << i << " ";
+	}
+	cout << "\n";
+
+	return 0;
+}
